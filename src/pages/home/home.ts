d418f0cd24bcb9d1a574b9d6623e0e9b629f96be
Gianmaria Leoni <gianmaria.leoni@gmail.com>
2016-12-12 10:40:07 +0100

    Structure complete

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,8 +10,12 @@ export class HomePage {
 
     @ViewChild('slides') slides: any;
 
+    hasAnswered: boolean = false;
+    score: number = 0;
+    life: number = 3;
+
     slideOptions: any;
-    flashCardFlipped: boolean = false;
+    questions: any;
 
     constructor(public navCtrl: NavController, public dataService: Data) {
 
@@ -23,10 +27,70 @@ export class HomePage {
 
     ionViewDidLoad() {
 
+        this.dataService.load().then((data) => {
+
+            data.map((question) => {
+
+                let originalOrder = question.answers;
+                question.answers = this.randomizeAnswers(originalOrder);
+                return question;
+
+            });
+
+            this.questions = data;
+
+        });
+
     }
 
-    selectAnswer(){
-        this.flashCardFlipped = true;
+    nextSlide(){
+        this.slides.slideNext();
+    }
+
+    selectAnswer(answer, question){
+
+        this.hasAnswered = true;
+        answer.selected = true;
+        question.flashCardFlipped = true;
+
+        if(answer.correct){
+            this.score++;
+        }else{
+            this.life--;
+        }
+
+        setTimeout(() => {
+
+          this.hasAnswered = false;
+          answer.selected = false;
+          question.flashCardFlipped = false;
+
+          if (this.life === 0) {
+            this.restartQuiz();
+          }else{
+            this.nextSlide();
+          }
+
+        }, 3000);
+    }
+
+    randomizeAnswers(rawAnswers: any[]): any[] {
+
+        for (let i = rawAnswers.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = rawAnswers[i];
+            rawAnswers[i] = rawAnswers[j];
+            rawAnswers[j] = temp;
+        }
+
+        return rawAnswers;
+
+    }
+
+    restartQuiz(){
+        this.score = 0;
+        this.life = 3;
+        this.slides.slideTo(1, 1000);
     }
 
 }
--- src/providers/data.ts
+++ src/providers/data.ts
@@ -2,17 +2,30 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the Data provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class Data {
 
-  constructor(public http: Http) {
-    console.log('Hello Data Provider');
-  }
+    data: any;
+
+    constructor(public http: Http) {
+
+    }
+
+    load(){
+
+        if(this.data){
+            return Promise.resolve(this.data);
+        }
+
+        return new Promise(resolve => {
+
+            this.http.get('assets/data/questions.json').map(res => res.json()).subscribe(data => {
+                this.data = data.questions;
+                resolve(this.data);
+            });
+
+        });
+
+    }
 
 }
